493a9f2715d0d3bd3559b3123124702cd88dc38b
Bruno <brunodaniel.elias@gmail.com>
2024-04-22 16:27:31 -0300

    add: value object slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,23 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  // Receives a string and returns a Slug instance
+  // Example: "Hello World  " => "hello-world"
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize("NFKD")
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "")
+      .replace(/_/g, "-")
+      .replace(/--+/g, "-")
+      .replace(/-$/g, "");
+
+    return new Slug(slugText);
+  }
+}
