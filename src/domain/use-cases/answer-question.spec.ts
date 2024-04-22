c9b159de77ce230a050d6b06b087f6878977f6a6
Bruno <brunodaniel.elias@gmail.com>
2024-04-22 15:07:43 -0300

    external dependencies

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,15 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const mockAnswersRepository: AnswersRepository = {
+  create: async () => {},
+};
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(mockAnswersRepository);
+
+  const answer = await answerQuestion.execute({
     instructorId: "1",
     questionId: "2",
     content: "This is the answer",
