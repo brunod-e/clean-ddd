1da7fda683a7cdaee5e5ee31074b4cdc064dfbf1
Bruno <brunodaniel.elias@gmail.com>
2024-04-23 14:39:31 -0300

    add: abstract entity create

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,31 @@
 import { Entity } from "../../core/entities/entity";
 import { UniqueEntityId } from "../../core/entities/unique-entity-id";
+import { Optional } from "../../core/types/optional";
 import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
+  authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
   title: string;
   slug: Slug;
   content: string;
-  authorId: UniqueEntityId;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    );
+
+    return question;
+  }
+}
